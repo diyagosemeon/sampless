db672916366a863cd36c3e1ca6c43a183b241d64
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2019-02-21 10:21:47 +0530

    Create sampless1.c

---
+++ sampless1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main() {
+    int first, diff, terms, value, sum=0, i;
+ printf("Enter the number of terms, first term, common difference in AP series\n");
+    scanf("%d %d %d", &terms,&first, &diff);
+    value = first;
+    printf("AP SERIES\n");
+    for(i = 0; i < terms; i++) {
+        printf("%d ", value);
+        sum += value;
+        value = value + diff;
+        }
+  printf("\nSum of the AP series till %d terms is %d\n", terms, sum);
+  getch();
+ return 0;
+}
